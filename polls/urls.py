9201ffe7d08c22e0d022e1349cff7886c0014f86
eclaros <eddyclarosc@gmail.com>
2022-10-14 12:00:11 -0400

    creacion de las primeras urls clase 21

--- polls/urls.py
+++ polls/urls.py
@@ -3,4 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('<int:question_id>/', views.detail, name="detail"),            #ex: /polls/5/
+    path('results/<int:question_id>/', views.results, name="results"),  #ex: /polls/results/5/
+    path('vote/<int:question_id>/', views.vote, name="index"),          #ex: /polls/votes/5/
+    
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,4 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hola Mundo desde mi pc")
+    return HttpResponse("Estas en la página principal de premios platzy")
+
+
+def detail(request,question_id):
+    return HttpResponse(f"Estás viendo la pregunta numero {question_id}")
+
+
+def results(request,question_id):
+    return HttpResponse(f"Estás viendo los resultados de la pregunta numero {question_id}")
+
+
+def vote(request,question_id):
+    return HttpResponse(f"Estás votando a la pregunta numero {question_id}")
+
