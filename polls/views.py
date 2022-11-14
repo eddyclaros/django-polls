6a11afae5b535bffebaea20337f0abb6a1194422
Eddy Claros <eclaros@segurosmsc.com>
2022-11-14 11:22:12 -0400

    avances

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from polls.models import Question
+
 # Create your views here.
 def index(request):
     return HttpResponse("Hola Mundo")
