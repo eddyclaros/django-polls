45ead44bd3f5c7335b1a3044c57bc5d56a08dcce
Eddy Claros <eclaros@segurosmsc.com>
2022-11-14 12:33:06 -0400

    3 test probados

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from datetime import datetime, timedelta
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
@@ -9,14 +10,7 @@ class Question(models.Model):
         return self.question_text
     
     def was_published_recently(self):
-        return self.pub_date >= timezone.now - datetime.timedelta(days=1)
-
-    def __str__(self):
-        return self.question_text
-    
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
+        return timezone.now() >= self.pub_date >= timezone.now() - timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question,on_delete = models.CASCADE)
--- polls/tests.py
+++ polls/tests.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
+from datetime import datetime,timedelta
 
 from .models import Question
 # Create your tests here.
@@ -9,9 +10,25 @@ from .models import Question
 #se testean modelos o vistas
 class QuestionModelTests(TestCase):
 
+    def setUp(self):
+        self.question = Question(question_text = "Quien es el mejor course director de platzy")
+
     def test_was_publish_recently_with_future_questions(self):
         """ was_published_recently returns False for question whose pub_date is in the future """
-        time = timezone.now() + datetime.timedelta(days = 30)
-        future_question = Question(question_text = "Quien es el mejor course director de platzy",pub_date = time)
-        self.assertIs(future_question.was_published_recently(),False)
-        
+        time = timezone.now() + timedelta(days = 30)
+        self.question.pub_date = time
+        self.assertIs(self.question.was_published_recently(),False)
+
+    def test_was_publish_recently_with_present_questions(self):
+        """ was_published_recently returns True for question whose pub_date is now """
+        time = timezone.now() - timedelta(hours = 23)
+        self.question.pub_date = time
+        self.assertIs(self.question.was_published_recently(),True)
+
+    def test_was_publish_recently_with_past_questions(self):
+        """ was_published_recently returns False for question whose pub_date is in the past """
+        time = timezone.now() - timedelta(days=1, minutes=1)
+        self.question.pub_date = time
+        self.assertIs(self.question.was_published_recently(),False)
+
+
