de83f4ed4e4f30ba7abb9033a5adc1166cb225b8
Eddy Claros <eclaros@segurosmsc.com>
2023-02-06 15:23:58 -0400

    leccion 21 django basico

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,17 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_list_or_404
+from django.urls import reverse
+from django.views import generic
 
-from polls.models import Question
+from polls.models import Question, Choice
+
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Estas en la página principal de premios platzy")
+    latest_question_list = Question.objects.all()
+    return render(request,"polls/index.html",{"latest_question_list":latest_question_list})
+    #return HttpResponse("Estas en la página principal de premios platzy")
 
 
 def detail(request,question_id):
@@ -18,4 +24,13 @@ def results(request,question_id):
 
 def vote(request,question_id):
     return HttpResponse(f"Estás votando a la pregunta numero {question_id}")
+
+
+# class IndexView(generic.ListView):
+#     template_name = "polls/index.html"
+#     context_object_name = "latest_question_list"
+
+#     def get_queryset(self):
+#         """ return the last five published questions """
+#         return Question.objects.filter("-pub_date")[:5]
 
