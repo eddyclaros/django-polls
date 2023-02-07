a3730fb008695f87ce3cdfa921a280fd5a96b7ef
Eddy Claros <eclaros@segurosmsc.com>
2023-02-07 12:05:34 -0400

    clase 24

--- polls/urls.py
+++ polls/urls.py
@@ -5,7 +5,7 @@ app_name = "polls"
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('<int:question_id>/detail/esto/', views.detail, name="detail"),            #ex: /polls/5/
+    path('<int:question_id>/', views.detail, name="detail"),            #ex: /polls/5/
     path('results/<int:question_id>/', views.results, name="results"),  #ex: /polls/results/5/
     path('vote/<int:question_id>/', views.vote, name="vote"),          #ex: /polls/votes/5/
     
