cf504e647bb76b7ace7fbe071bf24b26e4c5abe0
Eddy Claros <eclaros@segurosmsc.com>
2023-02-10 14:27:43 -0400

    clase 25

--- polls/views.py
+++ polls/views.py
@@ -28,7 +28,22 @@ def results(request,question_id):
 
 
 def vote(request,question_id):
-    return HttpResponse(f"Estás votando a la pregunta numero {question_id}")
+    #return HttpResponse(f"Estás votando a la pregunta numero {question_id}")
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError,Choice.DoesNotExist):
+        return render(request,"polls/detail.html",{
+            "question":question,
+            "error_message":"No elegiste una respuesta"
+        })
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        
+
+
 
 
 # class IndexView(generic.ListView):
