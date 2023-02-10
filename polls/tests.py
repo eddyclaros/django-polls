3737b32647d939b92971a788c64c33412e06b8e3
Eddy Claros <eclaros@segurosmsc.com>
2023-02-10 15:43:47 -0400

    test en views ok

--- polls/tests.py
+++ polls/tests.py
@@ -33,6 +33,15 @@ class QuestionModelTests(TestCase):
         self.assertIs(self.question.was_published_recently(),False)
 
 
+def create_question(question_text, days):
+    """
+    Create a question with the given "question_text", and published the given number
+    of days offset to now (negative for question published in the past,
+    positive for question that have yet to be published)
+    """
+    time = timezone.now() + timedelta(days = days)
+    return Question.objects.create(question_text=question_text,pub_date=time)
+
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
         """ if no question exist, an appropiate message is displayed """
@@ -40,6 +49,26 @@ class QuestionIndexViewTests(TestCase):
         self.assertEqual(response.status_code,200)
         self.assertContains(response,"No polls are available.")
         self.assertQuerysetEqual(response.context["latest_question_list"],[])
+    
+    def test_future_question(self):
+        """
+        Question with a pub_date in the future aren't displayed on te index page
+        """
+        create_question("Future question",days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertContains(response,"No polls are available.")
+        self.assertQuerysetEqual(response.context["latest_question_list"],[])
+
+
+    def test_past_question(self):
+        """
+        Questions with a pub_date in the past are displayed on the index page
+        """
+        question = create_question("Past Question",days=-10)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerysetEqual(response.context["latest_question_list"],[question])
+
+
 
 
 
