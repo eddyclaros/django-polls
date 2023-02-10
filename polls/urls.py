6842681f7b8b6510177ec13f5604d1e8e2980690
Eddy Claros <eclaros@segurosmsc.com>
2023-02-10 14:58:39 -0400

    class based views ok

--- polls/urls.py
+++ polls/urls.py
@@ -4,9 +4,12 @@ from django.urls import path
 app_name = "polls"
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('<int:question_id>/', views.detail, name="detail"),            #ex: /polls/5/
-    path('results/<int:question_id>/', views.results, name="results"),  #ex: /polls/results/5/
+    #path('', views.index, name="index"),
+    path('', views.IndexView.as_view(), name="index"),
+    #path('<int:question_id>/', views.detail, name="detail"),
+    path('<int:pk>/', views.DetailView.as_view(), name="detail"),             #ex: /polls/5/
+    #path('results/<int:question_id>/', views.results, name="results"),  #ex: /polls/results/5/
+    path('results/<int:pk>/', views.ResultView.as_view(), name="results"),
     path('vote/<int:question_id>/', views.vote, name="vote"),          #ex: /polls/votes/5/
     
 ]
--- polls/views.py
+++ polls/views.py
@@ -5,30 +5,46 @@ from django.views import generic
 
 from polls.models import Question, Choice
 
+# def index(request):
+#     latest_question_list = Question.objects.all()
+#     return render(request,"polls/index.html",{"latest_question_list":latest_question_list})
+#     #return HttpResponse("Estas en la página principal de premios platzy")
 
 
-# Create your views here.
-def index(request):
-    latest_question_list = Question.objects.all()
-    return render(request,"polls/index.html",{"latest_question_list":latest_question_list})
-    #return HttpResponse("Estas en la página principal de premios platzy")
+# def detail(request,question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request,"polls/detail.html",{
+#         "question":question
+#     })
+
+#     #return HttpResponse(f"Estás viendo la pregunta numero {question_id}")
 
 
-def detail(request,question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request,"polls/detail.html",{
-        "question":question
-    })
+# def results(request,question_id):
+#     #return HttpResponse(f"Estás viendo los resultados de la pregunta numero {question_id}")
+#     question = get_object_or_404(Question,pk=question_id)
+#     return render(request,"polls/results.html",{
+#         "question":question
+#     })
 
-    #return HttpResponse(f"Estás viendo la pregunta numero {question_id}")
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """ Return the last five published questions""" 
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def results(request,question_id):
-    #return HttpResponse(f"Estás viendo los resultados de la pregunta numero {question_id}")
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request,"polls/results.html",{
-        "question":question
-    })
+class DetailView(generic.DetailView):
+    model = Question
+    template_name='polls/detail.html'
+
+
+class ResultView(generic.DeleteView):
+    model = Question
+    template_name='polls/results.html'
+
 
 
 def vote(request,question_id):
