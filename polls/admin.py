a9761a3b95b0f7ba3033289ecf2b87d807447f3c
Eddy Claros <eclaros@segurosmsc.com>
2023-02-27 11:28:35 -0400

    respuestas en el administrador

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 
 from polls.models import Choice, Question
 
-admin.site.register([Question,Choice])
-#admin.site.register(Question)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields =  ["pub_date", "question_text"]
+    inlines = [ChoiceInline]
+
+#admin.site.register([Question,Choice])
+admin.site.register(Question,QuestionAdmin)
 #admin.site.register(Choice)
